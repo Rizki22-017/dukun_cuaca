aab137c2fc224290a5d7ca820350f60cbb876cf8
Rizki22-017 <126841212+Rizki22-017@users.noreply.github.com>
2025-04-27 00:49:23 +0700

    view-st

--- resources/views/surat/st.blade.php
+++ resources/views/surat/st.blade.php
@@ -2,59 +2,193 @@
 <html lang="id">
 <head>
     <meta charset="UTF-8">
-    <style>
-        body {
-            font-family: 'DejaVu Sans', sans-serif;
-            font-size: 12px;
-            line-height: 1.5;
-        }
-        .judul {
-            text-align: center;
-            font-weight: bold;
-            font-size: 16px;
-            margin-bottom: 20px;
-        }
-        .isi-surat {
-            margin: 20px 0;
-        }
-    </style>
+    <title>Surat Tugas</title>
 </head>
-<body>
+<body style="font-family: 'Times New Roman', Times, serif; font-size: 12pt; margin: 50px; line-height: 1.5;">
 
-    <div class="judul">SURAT TUGAS</div>
-
-    <p>Nomor:
-        {{-- {{ $data->nomor_surat }} --}}
-    </p>
-
-    <div class="isi-surat">
-        <p>Yang bertanda tangan di bawah ini:</p>
-        <p><strong>Nama Pejabat:</strong>
-            {{-- {{ $data->pejabat->nama_pegawai ?? '-' }} --}}
-        </p>
-
-        <p>Menugaskan kepada:</p>
-        <p><strong>Nama:</strong>
-            {{-- {{ $data->pegawai->nama_pegawai ?? '-' }} --}}
-        </p>
-        <p><strong>Maksud Tugas:</strong>
-            {{-- {{ $data->tugas }} --}}
-        </p>
-        <p><strong>Lokasi:</strong>
-            {{-- {{ $data->lokasi_berangkat }} → {{ $data->lokasi_tujuan }} --}}
-        </p>
-        <p><strong>Tanggal:</strong>
-            {{-- {{ \Carbon\Carbon::parse($data->tgl_mulai)->translatedFormat('d F Y') }} s/d {{ \Carbon\Carbon::parse($data->tgl_selesai)->translatedFormat('d F Y') }} --}}
-        </p>
+    <div style="text-align: center;">
+        <span style="text-decoration: underline; font-weight: bold; font-size: 14pt;">SURAT TUGAS</span><br>
+        <div style="margin-top: 8px;">
+            NOMOR: e.B/ME.02.04/016/KBB2/IX/2024
+        </div>
     </div>
 
-    <br><br>
-    <p style="text-align:right;">Padang,
-        {{-- {{ \Carbon\Carbon::now()->translatedFormat('d F Y') }} --}}
-    </p>
-    <p style="text-align:right;">
-        {{-- {{ $data->pejabat->nama_pegawai ?? '-' }} --}}
-    </p>
+    <div style="margin-top: 24px;">
+        Yang bertanda tangan di bawah ini:
+        <table style="width: 100%; margin-top: 8px; border-collapse: collapse;">
+            <tr>
+                <td style="width: 30%; vertical-align: top; padding-bottom: 4px;">Nama</td>
+                <td style="width: 2%; vertical-align: top;">:</td>
+                <td style="width: 68%; text-align: justify;">Hartanto, S.T., M.M.</td>
+            </tr>
+            <tr>
+                <td style="vertical-align: top; padding-bottom: 4px;">NIP</td>
+                <td style="vertical-align: top;">:</td>
+                <td style="text-align: justify;">197304181995031001</td>
+            </tr>
+            <tr>
+                <td style="vertical-align: top; padding-bottom: 4px;">Pangkat/Golongan</td>
+                <td style="vertical-align: top;">:</td>
+                <td style="text-align: justify;">Pembina Utama Muda / IV-c</td>
+            </tr>
+            <tr>
+                <td style="vertical-align: top; padding-bottom: 4px;">Jabatan</td>
+                <td style="vertical-align: top;">:</td>
+                <td style="text-align: justify;">Kepala Balai Besar</td>
+            </tr>
+            <tr>
+                <td style="vertical-align: top;">Unit Organisasi</td>
+                <td style="vertical-align: top;">:</td>
+                <td style="text-align: justify;">Balai Besar Meteorologi, Klimatologi, dan Geofisika Wilayah II</td>
+            </tr>
+        </table>
+    </div>
+
+    <div style="margin-top: 24px;">
+        Dengan ini memberikan tugas kepada:
+        <table style="width: 100%; margin-top: 8px; border-collapse: collapse;">
+            <tr>
+                <td style="width: 5%; vertical-align: top;">1.</td>
+                <td style="width: 95%;">
+                    <table style="width: 100%; border-collapse: collapse;">
+                        <tr>
+                            <td style="width: 25%; vertical-align: top; padding-bottom: 4px;">Nama</td>
+                            <td style="width: 2%; vertical-align: top;">:</td>
+                            <td style="width: 68%; text-align: justify;">Lina Adrianti, S.Si.</td>
+                        </tr>
+                        <tr>
+                            <td style="vertical-align: top; padding-bottom: 4px;">NIP</td>
+                            <td style="vertical-align: top;">:</td>
+                            <td style="text-align: justify;">198903012010122001</td>
+                        </tr>
+                        <tr>
+                            <td style="vertical-align: top; padding-bottom: 4px;">Pangkat/Gol.</td>
+                            <td style="vertical-align: top;">:</td>
+                            <td style="text-align: justify;">Penata Tk. I / III-d</td>
+                        </tr>
+                        <tr>
+                            <td style="vertical-align: top; padding-bottom: 4px;">Jabatan</td>
+                            <td style="vertical-align: top;">:</td>
+                            <td style="text-align: justify;">PMG Muda</td>
+                        </tr>
+                        <tr>
+                            <td style="vertical-align: top;">Unit Organisasi</td>
+                            <td style="vertical-align: top;">:</td>
+                            <td style="text-align: justify;">Balai Besar Meteorologi, Klimatologi, dan Geofisika Wilayah II</td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td style="vertical-align: top;">2.</td>
+                <td>
+                    <table style="width: 100%; border-collapse: collapse;">
+                        <tr>
+                            <td style="width: 25%; vertical-align: top; padding-bottom: 4px;">Nama</td>
+                            <td style="width: 2%; vertical-align: top;">:</td>
+                            <td style="width: 68%; text-align: justify;">Isnisa Citra Fiary, S.Tr.</td>
+                        </tr>
+                        <tr>
+                            <td style="vertical-align: top; padding-bottom: 4px;">NIP</td>
+                            <td style="vertical-align: top;">:</td>
+                            <td style="text-align: justify;">199111032010122001</td>
+                        </tr>
+                        <tr>
+                            <td style="vertical-align: top; padding-bottom: 4px;">Pangkat/Gol.</td>
+                            <td style="vertical-align: top;">:</td>
+                            <td style="text-align: justify;">Penata / III-c</td>
+                        </tr>
+                        <tr>
+                            <td style="vertical-align: top; padding-bottom: 4px;">Jabatan</td>
+                            <td style="vertical-align: top;">:</td>
+                            <td style="text-align: justify;">PMG Muda</td>
+                        </tr>
+                        <tr>
+                            <td style="vertical-align: top;">Unit Organisasi</td>
+                            <td style="vertical-align: top;">:</td>
+                            <td style="text-align: justify;">Balai Besar Meteorologi, Klimatologi, dan Geofisika Wilayah II</td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td style="vertical-align: top;">3.</td>
+                <td>
+                    <table style="width: 100%; border-collapse: collapse;">
+                        <tr>
+                            <td style="width: 25%; vertical-align: top; padding-bottom: 4px;">Nama</td>
+                            <td style="width: 2%; vertical-align: top;">:</td>
+                            <td style="width: 68%; text-align: justify;">Abun Rohibun Anwar</td>
+                        </tr>
+                        <tr>
+                            <td style="vertical-align: top; padding-bottom: 4px;">NIP</td>
+                            <td style="vertical-align: top;">:</td>
+                            <td style="text-align: justify;">197908302000031001</td>
+                        </tr>
+                        <tr>
+                            <td style="vertical-align: top; padding-bottom: 4px;">Pangkat/Gol.</td>
+                            <td style="vertical-align: top;">:</td>
+                            <td style="text-align: justify;">Penata / III-c</td>
+                        </tr>
+                        <tr>
+                            <td style="vertical-align: top; padding-bottom: 4px;">Jabatan</td>
+                            <td style="vertical-align: top;">:</td>
+                            <td style="text-align: justify;">PMG Penyelia</td>
+                        </tr>
+                        <tr>
+                            <td style="vertical-align: top;">Unit Organisasi</td>
+                            <td style="vertical-align: top;">:</td>
+                            <td style="text-align: justify;">Balai Besar Meteorologi, Klimatologi, dan Geofisika Wilayah II</td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+    </div>
+
+    <div style="margin-top: 24px;">
+        Untuk melaksanakan:
+        <table style="width: 100%; margin-top: 8px; border-collapse: collapse;">
+            <tr>
+                <td style="width: 30%; vertical-align: top; padding-bottom: 4px;">Tugas</td>
+                <td style="width: 2%; vertical-align: top;">:</td>
+                <td style="width: 68%; text-align: justify;">Kegiatan Simulasi Evakuasi Mandiri Pasar Cisoka</td>
+            </tr>
+            <tr>
+                <td style="vertical-align: top;">Selama</td>
+                <td style="vertical-align: top;">:</td>
+                <td style="text-align: justify;">19 September 2024 - 20 September 2024</td>
+            </tr>
+            <tr>
+                <td style="vertical-align: top;">Lokasi</td>
+                <td style="vertical-align: top;">:</td>
+                <td style="text-align: justify;">Pasar Cisoka, Jl. Raya Cisoka, Kec. Cisoka, Kabupaten Tangerang, Banten</td>
+            </tr>
+            <tr>
+                <td style="vertical-align: top;">Tanggal</td>
+                <td style="vertical-align: top;">:</td>
+                <td style="text-align: justify;">19 September 2024 s.d. 20 September 2024</td>
+            </tr>
+            <tr>
+                <td style="vertical-align: top;">Sumber Dana</td>
+                <td style="vertical-align: top;">:</td>
+                <td style="text-align: justify;">DIPA Tahun Anggaran 2024 Balai Besar Meteorologi, Klimatologi, dan Geofisika Wilayah II, Nomor: SP DIPA-075.01.2.437783/2024, tanggal 24 November 2023.</td>
+            </tr>
+        </table>
+    </div>
+
+    <div style="margin-top: 48px; text-align: right;">
+        Tangerang Selatan, 19 September 2024<br>
+        Kepala,<br><br><br><br>
+        <span style="font-weight: bold; text-decoration: underline;">Hartanto, S.T., M.M.</span>
+    </div>
+
+    <div style="margin-top: 48px;">
+        <hr>
+        <small><i>
+            Dokumen ini telah ditandatangani secara elektronik menggunakan sertifikat elektronik yang diterbitkan oleh Balai Sertifikasi Elektronik (BSrE), Badan Siber dan Sandi Negara.
+        </i></small>
+    </div>
 
 </body>
 </html>

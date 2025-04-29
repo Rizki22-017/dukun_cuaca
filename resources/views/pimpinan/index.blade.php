2d45d51f87c896b188b9710d16b4b7f38daff6af
Hirezkii <hirezkiit@gmail.com>
2025-04-30 01:03:28 +0700

    hapu komen tab

--- resources/views/pimpinan/index.blade.php
+++ resources/views/pimpinan/index.blade.php
@@ -27,135 +27,58 @@
         </script>
     @endif
 
-    <div class="container mt-4">
+    <div class="mb-4 mt-4">
+        <a href={{ route('Pimpinan.create') }}><button type="button" class="btn btn-success">Tambah
+                Data</button></a>
+    </div>
+    <div class="table-wrapper">
+        <table class="table table-bordered text-center align-middle">
+            <thead class="table-secondary">
+                <tr>
+                    <th scope="col">NIP</th>
+                    <th scope="col">Nama Pimpinan</th>
+                    <th scope="col">Pangkat Golongan</th>
+                    <th scope="col">Jabatan</th>
+                    <th scope="col">Bagian Kerja</th>
+                    <th scope="col">Tanggal Lahir</th>
+                    <th scope="col">Tipe Pimpinan</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($pegawais as $pegawai)
+                    <tr>
+                        <td>{{ $pegawai->nip }}</td>
+                        <td>{{ $pegawai->nama_pegawai }}</td>
+                        <td>{{ $pegawai->pangkat_golongan }}</td>
+                        <td>{{ $pegawai->jabatan }}</td>
+                        <td>{{ $pegawai->bagian_kerja }}</td>
+                        <td>{{ $pegawai->tanggal_lahir }}</td>
+                        <td>
+                            @if (is_array($pegawai->wewenang))
+                                {{ implode(', ', $pegawai->wewenang) }}
+                            @else
+                                {{ $pegawai->wewenang }}
+                            @endif
+                        </td>
+                        <td>
+                            <a href="{{ route('Pimpinan.edit', $pegawai->id_pegawai) }}"
+                                class="btn btn-outline-primary btn-sm">Edit</a>
 
-        {{-- <ul class="nav nav-tabs mb-3" id="myTab" role="tablist">
-            <li class="nav-item" role="presentation">
-                <button class="nav-link active" id="pst-tab" data-bs-toggle="tab" data-bs-target="#pst" type="button"
-                    role="tab" aria-controls="pst" aria-selected="true"><b>Pimpinan Surat Tugas</b></button>
-            </li>
-            <li class="nav-item" role="presentation">
-                <button class="nav-link" id="pspd-tab" data-bs-toggle="tab" data-bs-target="#pspd" type="button"
-                    role="tab" aria-controls="pspd" aria-selected="false"><b>Pimpinan Surat Perjalanan
-                        Dinas</b></button>
-            </li>
-        </ul> --}}
+                            <button class="btn btn-outline-danger btn-sm"
+                                onclick="confirmDelete({{ $pegawai->id_pegawai }})">Downgrade</button>
 
-        <div class="tab-content" id="myTabContent">
-            <div class="tab-pane fade show active" id="pst" role="tabpanel" aria-labelledby="pst-tab">
-
-                <div class="mb-4 mt-4">
-                    <a href={{ route('Pimpinan.create') }}><button type="button" class="btn btn-success">Tambah
-                            Data</button></a>
-                </div>
-
-                <div class="mb-2">
-                    <input type="text" class="form-control" placeholder="🔍 All fields">
-                </div>
-                <div class="table-wrapper">
-                    <table class="table table-bordered text-center align-middle">
-                        <thead class="table-secondary">
-                            <tr>
-                                <th scope="col">NIP</th>
-                                <th scope="col">Nama Pimpinan</th>
-                                <th scope="col">Pangkat Golongan</th>
-                                <th scope="col">Jabatan</th>
-                                <th scope="col">Bagian Kerja</th>
-                                <th scope="col">Tanggal Lahir</th>
-                                <th scope="col">Tipe Pimpinan</th>
-                                <th scope="col">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($pegawais as $pegawai)
-                                <tr>
-                                    <td>{{ $pegawai->nip }}</td>
-                                    <td>{{ $pegawai->nama_pegawai }}</td>
-                                    <td>{{ $pegawai->pangkat_golongan }}</td>
-                                    <td>{{ $pegawai->jabatan }}</td>
-                                    <td>{{ $pegawai->bagian_kerja }}</td>
-                                    <td>{{ $pegawai->tanggal_lahir }}</td>
-                                    <td>
-                                        @if(is_array($pegawai->wewenang))
-                                            {{ implode(', ', $pegawai->wewenang) }}
-                                        @else
-                                            {{ $pegawai->wewenang }}
-                                        @endif
-                                    </td>
-                                    <td>
-                                        <a href="{{ route('Pimpinan.edit', $pegawai->id_pegawai) }}"
-                                            class="btn btn-outline-primary btn-sm">Edit</a>
-
-                                        <button class="btn btn-outline-danger btn-sm"
-                                            onclick="confirmDelete({{ $pegawai->id_pegawai }})">Downgrade</button>
-
-                                        <form id="delete-form-{{ $pegawai->id_pegawai }}"
-                                            action="{{ route('Pimpinan.destroy', $pegawai->id_pegawai) }}" method="POST"
-                                            style="display: none;">
-                                            @csrf
-                                            @method('DELETE')
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-
-            {{-- <div class="tab-pane fade" id="pspd" role="tabpanel" aria-labelledby="pspd-tab">
-
-                <div class="mb-4 mt-4">
-                    <a href={{ route('Pimpinan.createSpd') }}><button type="button" class="btn btn-success">Tambah
-                            Data</button></a>
-                </div>
-
-                <div class="mb-2">
-                    <input type="text" class="form-control" placeholder="🔍 All fields">
-                </div>
-                <div class="table-wrapper">
-                    <table class="table table-bordered text-center align-middle">
-                        <thead class="table-secondary">
-                            <tr>
-                                <th scope="col">NIP</th>
-                                <th scope="col">Nama Pimpinan SPD</th>
-                                <th scope="col">Pangkat Golongan</th>
-                                <th scope="col">Jabatan</th>
-                                <th scope="col">Bagian Kerja</th>
-                                <th scope="col">Tanggal Lahir</th>
-                                <th scope="col">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($pimpinanspds as $pspd)
-                                <tr>
-                                    <td>{{ $pspd->pegawai->nip }}</td>
-                                    <td>{{ $pspd->pegawai->nama_pegawai }}</td>
-                                    <td>{{ $pspd->pegawai->pangkat_golongan }}</td>
-                                    <td>{{ $pspd->pegawai->jabatan }}</td>
-                                    <td>{{ $pspd->pegawai->bagian_kerja }}</td>
-                                    <td>{{ $pspd->pegawai->tanggal_lahir }}</td>
-                                    <td>
-                                        <a href="{{ route('Pimpinan.editSpd', $pspd->id_pimpinan_spd) }}"
-                                            class="btn btn-outline-primary btn-sm">Edit</a>
-
-                                        <button class="btn btn-outline-danger btn-sm"
-                                            onclick="confirmDelete({{ $pspd->id_pimpinan_spd }})">Delete</button>
-
-                                        <form id="delete-form-{{ $pspd->id_pimpinan_spd }}"
-                                            action="{{ route('Pimpinan.destroySpd', $pspd->id_pimpinan_spd) }}"
-                                            method="POST" style="display: none;">
-                                            @csrf
-                                            @method('DELETE')
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div> --}}
-        </div>
+                            <form id="delete-form-{{ $pegawai->id_pegawai }}"
+                                action="{{ route('Pimpinan.destroy', $pegawai->id_pegawai) }}" method="POST"
+                                style="display: none;">
+                                @csrf
+                                @method('DELETE')
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 
     <script>
